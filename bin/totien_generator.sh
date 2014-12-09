fdd228a35b699794f3a5f0f1892498d5372c4666
Manuel Pineda <manuel.felipe.pineda@gmail.com>
2014-12-08 20:12:52 -0500

    Totient generator

--- bin/totien_generator.sh
+++ bin/totien_generator.sh
@@ -5,11 +5,31 @@ if [ "$#" -lt 1 ]; then
   exit 1
 fi
 
-split -b 256000 $1 current
+
+CHUNK_SIZE=256000
+split -b $CHUNK_SIZE $1 current
+
+TRACKER=localhost
+if [ "$#" -gt 1 ]; then
+  TRACKER=$2
+fi
+
+PORT=6666
+
+if [ "$#" -gt 2 ]; then
+  PORT=$3
+fi
+
+TARGET=$(echo $1 | awk -F "/" '{print $2}').totient
+echo $TRACKER:$PORT > $TARGET
+echo $1 >> $TARGET
+echo $CHUNK_SIZE >> $TARGET
+echo $(du -b $1 | awk '{ print $1 }') >> $TARGET
 
 for i in current*; do
   name=$(sha1sum $i | awk '{ print $1 }')
   echo $name >> pieces/list
+  echo $name >> $TARGET
   mv $i pieces/$name
 done
 
